d800500fd7e99d6e4fc16ff29b0cb64cae132405
北省 <jih6666@126.com>
2023-01-03 12:03:59 +0800

    修改分享界面

--- components/rule/index.js
+++ components/rule/index.js
@@ -11,8 +11,8 @@ Component({
   methods: {
     previewImg() {
       wx.previewImage({
-        current: 'https://api.xingzou.art/download/fanwei.jpg', // 当前显示图片的http链接
-        urls: ['https://api.xingzou.art/download/fanwei.jpg'] // 需要预览的图片http链接列表
+        current: 'http://www.anqi520.com/upload/2023/01/%E5%9C%B0%E5%9B%BE-c9c4920a39fd4b649df7e52ab4d914f0.jpg', // 当前显示图片的http链接
+        urls: ['http://www.anqi520.com/upload/2023/01/%E5%9C%B0%E5%9B%BE-c9c4920a39fd4b649df7e52ab4d914f0.jpg'] // 需要预览的图片http链接列表
       });
     },
   },
